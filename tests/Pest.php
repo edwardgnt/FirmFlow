ce87c4dafbdadf00f4b3137158375d919a847434
edwardgnt <edwardgnt@gmail.com>
2026-04-19 11:43:00 -0700

    Fix CI test isolation with RefreshDatabase

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,11 +15,11 @@ use Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
- // ->use(RefreshDatabase::class)
+    ->use(RefreshDatabase::class)
     ->in('Feature');
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------- 
 | Expectations
 |--------------------------------------------------------------------------
 |
@@ -29,9 +29,9 @@ pest()->extend(TestCase::class)
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+// expect()->extend('toBeOne', function () {
+//     return $this->toBe(1);
+// });
 
 /*
 |--------------------------------------------------------------------------
